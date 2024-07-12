f90475a19b9c098c6105fb46e061593c32969552
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-12 17:02:07 +0530

    lm

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import {FaReact} from 'react-icons/fa';
+import React, { useState } from 'react';
+import {FaBars,FaReact} from 'react-icons/fa';
+import {HiX} from 'react-icons/hi';
 import { Link } from 'react-router-dom';
-
+import './styles.scss';
 const data = [
     {
         label: 'HOME',
@@ -30,6 +31,10 @@ const data = [
 ];
 
 const Navbar = () => {
+    const [toggleIcon,setToggleIcon] = useState(false);
+    const handleToggleIcon = () =>{
+        setToggleIcon(!toggleIcon);
+    };
     return (
         <div>
             <nav className="navbar">
@@ -37,6 +42,23 @@ const Navbar = () => {
                     <Link to={"/"} className="navbar_container_logo">
                         <FaReact size={30} />
                     </Link>
+                </div>
+                <ul className="navbar_container_menu">
+                    {
+                        data.map((item,key)=>(
+                            <li key = {key}className="navbar_container_menu_item">
+                                <Link className = "navbar_container_menu_item_links" to= {item.to}>
+                                 {item.label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className="nav-icon" onClick={handleToggleIcon}>
+                 {
+                    toggleIcon ? <HiX size={30}/> : <FaBars size ={30}/>
+                 }
+
                 </div>
             </nav>
         </div>
