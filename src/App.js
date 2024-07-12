80cd2c10cd6298834ae5bb3d7e9dbe83eded7222
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-13 00:13:27 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import {Routes, Route } from 'react-router-dom';
-import Particles from "react-tsparticles";
+import { Routes, Route } from 'react-router-dom';
+import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import Home from './containers/home';
 import About from './containers/about';
@@ -12,20 +12,107 @@ import Contact from './containers/contact';
 import Navbar from './components/navBar';
 
 function App() {
-  const handleInit = async(main)=>{
-    await loadFull(main)
-  }
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
-    {/*Particles js*/}
-    <Particles id ="particles" init ={handleInit}/>
-     {/*Navbar */}
-     <Navbar/>
+      {/*Particles js*/}
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#0d47a1',
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#ffffff',
+            },
+            links: {
+              color: '#ffffff',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      {/*Navbar */}
+      <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
-        <Route path="/skills" element={<Skills />} /> 
+        <Route path="/skills" element={<Skills />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
@@ -34,4 +121,5 @@ function App() {
 }
 
 export default App;
+
 
