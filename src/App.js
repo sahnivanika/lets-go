ba78f5dad2b5346c64b98a81bf0fa70d6bcc264e
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-13 00:39:51 +0530

    v

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Particles from 'react-tsparticles';
@@ -13,53 +12,19 @@ import Navbar from './components/navBar';
 
 function App() {
   const particlesInit = async (main) => {
+    console.log(main); // This should log the main object
     await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
   };
 
   return (
     <div className="App">
-      {/* Particles js */}
       <Particles
         id="tsparticles"
         init={particlesInit}
-        loaded={particlesLoaded}
         options={{
           background: {
             color: {
               value: "#0d47a1",
-            },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
             },
           },
           particles: {
@@ -73,22 +38,11 @@ function App() {
               opacity: 0.5,
               width: 1,
             },
-            collisions: {
-              enable: true,
-            },
             move: {
-              direction: "none",
               enable: true,
-              outModes: "bounce",
-              random: false,
               speed: 6,
-              straight: false,
             },
             number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
               value: 80,
             },
             opacity: {
@@ -98,14 +52,11 @@ function App() {
               type: "circle",
             },
             size: {
-              random: true,
               value: 5,
             },
           },
-          detectRetina: true,
         }}
       />
-      {/* Navbar */}
       <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
@@ -120,5 +71,6 @@ function App() {
 }
 
 export default App;
+
 
 
