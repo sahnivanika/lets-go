b99f705888d2e18d43987098edceab72f2bbfb7a
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-13 02:57:45 +0530

    v

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
 import Home from './containers/home';
 import About from './containers/about';
 import Resume from './containers/resume';
@@ -11,52 +10,9 @@ import Contact from './containers/contact';
 import Navbar from './components/navBar';
 
 function App() {
-  const particlesInit = async (main) => {
-    console.log(main); // This should log the main object
-    await loadFull(main);
-  };
-
   return (
     <div className="App">
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        options={{
-          background: {
-            color: {
-              value: "#0d47a1",
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 6,
-            },
-            number: {
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: 5,
-            },
-          },
-        }}
-      />
+        {/*Navbar */}
       <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
@@ -71,6 +27,5 @@ function App() {
 }
 
 export default App;
-
 
 
