18db0fb8bf9b7f523173c2dc49595ceba94a6820
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-13 18:50:36 +0530

    v

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import './styles.scss';
+import {Animate} from 'react-simple-animate'
 const Home = () => {
+
+  const navigate =useNavigate();
+  const handleNavigateToContactMePage=()=>{
+    navigate("/contact");
+  }
   return (
   <section id ="home" className='home'>
     <div className=" home_text-wrapper">
@@ -10,9 +17,20 @@ const Home = () => {
       Front end developer
     </h1>
     </div>
+    <Animate
+    play={1.5}
+    delay={1}
+    start={{
+      transform: 'translateY(550px)'
+    }}
+    end={{
+      transform:'translatex(0px)'
+    }}>
     <div className="home_contact-me">
-      <button>Let's talk</button>
+    <button onClick={handleNavigateToContactMePage}>Let's talk</button>
     </div>
+    </Animate>
+    
   </section>
   )
 };
